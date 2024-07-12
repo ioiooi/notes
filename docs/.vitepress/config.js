66ec2ac7f4c1db81d512ea355ea2d1a71c64577e
Eugen Mayer <eugen.mayer2@gmail.com>
2024-07-12 23:03:38 +0200

    reorganize notes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,62 +5,85 @@ export default {
   themeConfig: {
     sidebar: [
       {
-        text: "Miscellaneous",
-        items: [
-          {
-            text: "Node.js Environment Variables",
-            link: "/misc/nodejs-environment-variables",
-          },
-          { text: "Unix Time", link: "/misc/unix-time" },
-          { text: "Promises", link: "/misc/promisify-callback-functions" },
-          { text: "Database", link: "/misc/fundamental-database-concepts" },
-          { text: "Find and delete files", link: "/misc/find-and-delete-files" },
-          { text: "Deployment", link: "/misc/deployment" },
-          {
-            text: "Case conversion with `sed`",
-            link: "/misc/string-to-upper-sed-regex",
-          },
-          {
-            text: "EditorConfig Naming Conventions",
-            link: "/misc/editorconfig-naming-convention",
-          },
-          {
-            text: "Prefix Commit Messages",
-            link: "/misc/prefix-git-commit-messages-hook",
-          },
-          {
-            text: "Loading multiple ssh-keys at startup",
-            link: "/misc/loading-multiple-ssh-keys-on-osx-and-wsl2",
-          },
-          {
-            text: "Search, Count, and Sort File Matches using Regular Expression",
-            link: "/misc/search-count-sort-files-using-regex",
-          },
-          {
-            text: "Generate TOC for MD structures",
-            link: "/misc/generate-toc-script",
-          },
-          {
-            text: "WoW Macros",
-            link: "/misc/wow-macros",
-          },
-          {
-            text: "WoW Macros - Hunter",
-            link: "/misc/wow-macros-hunter",
-          },
-        ],
-      },
-      {
         text: "Android",
-        items: [{ text: "ArrayAdapter", link: "/android/arrayadapter" }],
+        items: [{ text: "ArrayAdapter", link: "/android/arrayadapter.md" }],
       },
       {
         text: "Books",
         items: [
-          { text: "Node.js in Action", link: "/books/nodejs-in-action" },
+          { text: "Node.js in Action", link: "/books/nodejs-in-action.md" },
           {
             text: "Secrets of the JavaScript Ninja",
-            link: "/books/secrets-of-the-javascript-ninja",
+            link: "/books/secrets-of-the-javascript-ninja.md",
+          },
+        ],
+      },
+      {
+        text: "CLI and Scripting",
+        items: [
+          {
+            text: "Case conversion via `sed`",
+            link: "/cli-scripting/case-conversion-sed-regex.md",
+          },
+          {
+            text: "Find and delete files",
+            link: "/cli-scripting/find-and-delete-files.md",
+          },
+          {
+            text: "Generate TOC for MD structures",
+            link: "/cli-scripting/generate-toc-script.md",
+          },
+          {
+            text: "Search, Count, and Sort File Matches using Regular Expression",
+            link: "/cli-scripting/search-count-sort-files-using-regex.md",
+          },
+        ],
+      },
+      {
+        text: "Dev Tools",
+        items: [
+          { text: "Deployment", link: "/dev-tools/deployment.md" },
+          {
+            text: "EditorConfig naming convention",
+            link: "/dev-tools/editorconfig-naming-convention.md",
+          },
+          {
+            text: "Loading multiple ssh-keys on OSX and WSL2",
+            link: "/dev-tools/loading-multiple-ssh-keys-on-osx-and-wsl2.md",
+          },
+          {
+            text: "Prefix Git Commit Messages Hook",
+            link: "/dev-tools/prefix-git-commit-messages-hook.md",
+          },
+        ],
+      },
+      {
+        text: "Gaming",
+        items: [
+          { text: "WoW Macros - Hunter", link: "/gaming/wow-macros-hunter.md" },
+          { text: "WoW Macros", link: "/gaming/wow-macros.md" },
+        ],
+      },
+      {
+        text: "Programming",
+        items: [
+          {
+            text: "Fundamental Database Concepts",
+            link: "/programming/fundamental-database-concepts.md",
+          },
+          { text: "Unix Time", link: "/programming/unix-time.md" },
+          {
+            text: "JavaScript",
+            items: [
+              {
+                text: "Node.js Environment Variables",
+                link: "/programming/javascript/nodejs-environment-variables.md",
+              },
+              {
+                text: "Promisify callback functions",
+                link: "/programming/javascript/promisify-callback-functions.md",
+              },
+            ],
           },
         ],
       },
--- docs/index.md
+++ docs/index.md
@@ -1,41 +1,49 @@
 # Hello VitePress
 
-## Miscellaneous
-
-[Node.js Environment Variables](/misc/nodejs-environment-variables.md)
-
-[Unix Time](/misc/unix-time.md)
-
-[Promisify callback functions](/misc/promisify-callback-functions.md)
-
-[Fundamental Database Concepts](/misc/fundamental-database-concepts.md)
-
-[Find and delete files](/misc/find-and-delete-files.md)
-
-[Deployment](/misc/deployment.md)
-
-[Case conversion via `sed`](/misc/string-to-upper-sed-regex.md)
-
-[EditorConfig naming convention](/misc/editorconfig-naming-convention.md)
-
-[Prefix Git Commit Messages Hook](/misc/prefix-git-commit-messages-hook.md)
-
-[Loading multiple ssh-keys on OSX and WSL2](/misc/loading-multiple-ssh-keys-on-osx-and-wsl2.md)
-
-[Search, Count, and Sort File Matches using Regular Expression](/misc/search-count-sort-files-using-regex.md)
-
-[Generate TOC for MD structures](/misc/generate-toc-script.md)
-
-[WoW Macros](/misc/wow-macros.md)
-
-[WoW Macros - Hunter](/misc/wow-macros-hunter.md)
-
 ## Android
 
 [ArrayAdapter](/android/arrayadapter.md)
 
-## Ebooks
+## Books
 
 [Node.js in Action](/books/nodejs-in-action.md)
 
 [Secrets of the JavaScript Ninja](/books/secrets-of-the-javascript-ninja.md)
+
+## CLI and Scripting
+
+[Case conversion via `sed`](/cli-scripting/cas-conversion-sed-regex.md)
+
+[Find and delete files](/cli-scripting/find-and-delete-files.md)
+
+[Generate TOC for MD structures](/cli-scripting/generate-toc-script.md)
+
+[Search, Count, and Sort File Matches using Regular Expression](/cli-scripting/search-count-sort-files-using-regex.md)
+
+## Dev Tools
+
+[Deployment](/dev-tools/deployment.md)
+
+[EditorConfig naming convention](/dev-tools/editorconfig-naming-convention.md)
+
+[Loading multiple ssh-keys on OSX and WSL2](/dev-tools/loading-multiple-ssh-keys-on-osx-and-wsl2.md)
+
+[Prefix Git Commit Messages Hook](/dev-tools/prefix-git-commit-messages-hook.md)
+
+## Gaming
+
+[WoW Macros - Hunter](/gaming/wow-macros-hunter.md)
+
+[WoW Macros](/gaming/wow-macros.md)
+
+## Programming
+
+[Fundamental Database Concepts](/programming/fundamental-database-concepts.md)
+
+[Unix Time](/programming/unix-time.md)
+
+### JavaScript
+
+[Node.js Environment Variables](/programming/javascript/nodejs-environment-variables.md)
+
+[Promisify callback functions](/programming/javascript/promisify-callback-functions.md)
